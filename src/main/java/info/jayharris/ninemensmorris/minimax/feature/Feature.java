d8ecce3378428941173bdca8b6751fa9beb194f3
Jay Harris <jaywritescode@users.noreply.github.com>
2019-04-05 14:34:23 -0400

    explain what a Feature is

--- src/main/java/info/jayharris/ninemensmorris/minimax/feature/Feature.java
+++ src/main/java/info/jayharris/ninemensmorris/minimax/feature/Feature.java
@@ -3,6 +3,14 @@ package info.jayharris.ninemensmorris.minimax.feature;
 import info.jayharris.ninemensmorris.Piece;
 import info.jayharris.ninemensmorris.minimax.MinimaxState;
 
+/**
+ * A quantitative feature of the board, or more precisely, of the state.
+ *
+ * Some examples might be the number of legal moves that the (state's) current
+ * player has available or the total point value of the current player's pieces
+ * on a chessboard, or a number representing the quality of the current player's
+ * defense, etc.
+ */
 public abstract class Feature {
 
     protected final Piece piece;
