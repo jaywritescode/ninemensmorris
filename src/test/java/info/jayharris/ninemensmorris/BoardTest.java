4fd56653177bbee7310ca10d54e4dc83d3215b11
Jay Harris <jaywritescode@users.noreply.github.com>
2019-11-03 16:18:09 -0500

    test Board#equals

--- src/test/java/info/jayharris/ninemensmorris/BoardTest.java
+++ src/test/java/info/jayharris/ninemensmorris/BoardTest.java
@@ -1,8 +1,8 @@
 package info.jayharris.ninemensmorris;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +29,77 @@ class BoardTest {
         assertThat(copy)
                 .as("Original board:\n%s\nis the same as copy:\n%s", original.pretty(), copy.pretty())
                 .isEqualTo(original);
+    }
+
+    @Nested
+    class Equals {
+        @Test
+        @DisplayName("two boards are #equals")
+        void testEquals() {
+            Board b1 = BoardBuilder.create()
+                    .withPiece("a7", Piece.WHITE)
+                    .withPiece("d6", Piece.BLACK)
+                    .withPiece("c5", Piece.BLACK)
+                    .withPiece("e5", Piece.BLACK)
+                    .withPiece("b4", Piece.BLACK)
+                    .withPiece("c4", Piece.WHITE)
+                    .withPiece("e4", Piece.WHITE)
+                    .withPiece("f4", Piece.BLACK)
+                    .withPiece("g4", Piece.WHITE)
+                    .withPiece("c3", Piece.WHITE)
+                    .withPiece("b2", Piece.WHITE)
+                    .withPiece("d2", Piece.BLACK)
+                    .build();
+            Board b2 = BoardBuilder.create()
+                    .withPiece("a7", Piece.WHITE)
+                    .withPiece("d6", Piece.BLACK)
+                    .withPiece("c5", Piece.BLACK)
+                    .withPiece("e5", Piece.BLACK)
+                    .withPiece("b4", Piece.BLACK)
+                    .withPiece("c4", Piece.WHITE)
+                    .withPiece("e4", Piece.WHITE)
+                    .withPiece("f4", Piece.BLACK)
+                    .withPiece("g4", Piece.WHITE)
+                    .withPiece("c3", Piece.WHITE)
+                    .withPiece("b2", Piece.WHITE)
+                    .withPiece("d2", Piece.BLACK)
+                    .build();
+
+            assertThat(b1.equals(b2)).isTrue();
+        }
+
+        @Test
+        @DisplayName("two boards are not #equals")
+        void testNotEquals() {
+            Board b1 = BoardBuilder.create()
+                    .withPiece("a7", Piece.WHITE)
+                    .withPiece("d6", Piece.BLACK)
+                    .withPiece("c5", Piece.BLACK)
+                    .withPiece("e5", Piece.BLACK)
+                    .withPiece("b4", Piece.BLACK)
+                    .withPiece("c4", Piece.WHITE)
+                    .withPiece("e4", Piece.WHITE)
+                    .withPiece("f4", Piece.BLACK)
+                    .withPiece("g4", Piece.WHITE)
+                    .withPiece("c3", Piece.WHITE)
+                    .withPiece("b2", Piece.WHITE)
+                    .withPiece("d2", Piece.BLACK)
+                    .build();
+            Board b2 = BoardBuilder.create()
+                    .withPiece("a7", Piece.WHITE)
+                    .withPiece("d6", Piece.BLACK)
+                    .withPiece("c5", Piece.BLACK)
+                    .withPiece("e5", Piece.WHITE)
+                    .withPiece("b4", Piece.BLACK)
+                    .withPiece("c4", Piece.WHITE)
+                    .withPiece("f4", Piece.BLACK)
+                    .withPiece("g4", Piece.WHITE)
+                    .withPiece("c3", Piece.WHITE)
+                    .withPiece("b2", Piece.WHITE)
+                    .withPiece("d2", Piece.BLACK)
+                    .build();
+
+            assertThat(b1.equals(b2)).isFalse();
+        }
     }
 }
